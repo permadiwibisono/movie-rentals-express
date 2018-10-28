6e6a2c45e94d46615403435ddf109cd39e9956eb
Permadi Wibisono <mbapewe@gmail.com>
2018-10-28 18:19:12 +0700

    Moving validateGenre func inside Genre Model

--- models/genre.js
+++ models/genre.js
@@ -1,5 +1,14 @@
+const Joi = require('joi');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required()
+  };
+
+  return Joi.validate(genre, schema);
+}
 
 const genreSchema = new Schema({
   name: {
@@ -12,5 +21,6 @@ const genreSchema = new Schema({
 
 module.exports = {
   genreSchema,
-  Genre: mongoose.model('Genre', genreSchema)
+  Genre: mongoose.model('Genre', genreSchema),
+  validate: validateGenre
 };
