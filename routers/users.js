159dbe7a094414f1114d7c0f21bf79f61dc53651
Permadi Wibisono <mbapewe@gmail.com>
2018-11-02 09:47:34 +0700

    Adding lodash, hide password field to the response with lodash

--- routers/users.js
+++ routers/users.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 const { User, validate } = require('../models/user');
@@ -10,13 +11,9 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if(user) return res.status(422).send('User already registered.');
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
   await user.save();
-  res.send(user);
+  res.send(_.pick(user, ['name', 'email', '_id']));
 });
 
 module.exports = router;
