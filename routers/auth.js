92019dcf86f0f51f2865c6d8508e5526ab8c5163
Permadi Wibisono <mbapewe@gmail.com>
2018-11-07 02:35:24 +0700

    Testing authentication user

--- routers/auth.js
+++ routers/auth.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const { User, validateLogin } = require('../models/user');
-
 
 router.post('/', async (req, res) => {
   const { error } = validateLogin(req.body);
   if (error) return res.status(422).send(error.details[0].message);
 
-  const user = await User.findOne({
-    email: req.body.email,
-    password: req.body.password
-  });
-  res.send(user);
+  let user = await User.findOne({ email: req.body.email });
+  if(!user) return res.status(400).send("Invalid email or password");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if(!validPassword) return res.status(400).send("Invalid email or password");
+
+  res.send(true);
 });
 
 module.exports = router;
