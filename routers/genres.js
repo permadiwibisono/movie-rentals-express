79821378dccf33f157d30c948a480f10a8f977d6
Permadi Wibisono <mbapewe@gmail.com>
2018-11-14 14:42:00 +0700

    Protecting genres route must be logged can modify data

--- routers/genres.js
+++ routers/genres.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Joi = require('joi');
+const authMiddleware = require('../middlewares/auth');
 const router = express.Router();
 const { Genre, validate } = require('../models/genre');
 
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   res.send(genres);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', authMiddleware, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(422).send(error.details[0].message);
 
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
   res.send(genre);
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', authMiddleware, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(422).send(error.details[0].message);
 
@@ -30,7 +30,7 @@ router.put('/:id', async (req, res) => {
   res.send(genre);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', authMiddleware, async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
